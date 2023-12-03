56c59cb8911affe0851dc8b95c4cfbeb5263eb73
yuyizhong <sunny_law@hotmail.com>
2023-12-03 12:40:54 +0000

    change the allowed hosts name

--- tradepro/settings.py
+++ tradepro/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['trade-pro.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['trade-pro-4909851596e5.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
