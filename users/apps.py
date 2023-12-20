44d3602a6dee65791222b78f66c4850ac10d487e
yuyizhong <sunny_law@hotmail.com>
2023-12-20 14:43:09 +0000

    create signals and connect with the user model

--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class UserConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
+    
+    def ready(self):
+        #this connects the signals with the model
+        import users.signals
--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
+import uuid
+
 from django.db import models
 from custom_user.models import User
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE, null=True, blank=True)
@@ -8,6 +11,9 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length = 500, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
     
-    def __object__(self):
-        return  self.first_name
+    def __str__(self):
+        return  str(self.email)
