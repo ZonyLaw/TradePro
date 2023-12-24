14d7a81bcce3d1cdf87ba581cbf05215ac26ddb0
yuyizhong <sunny_law@hotmail.com>
2023-12-24 00:05:00 +0000

    add timezone to profile2

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.db import models
 from custom_user.models import User
+from timezone_field import TimeZoneField
 
 
 class Profile(models.Model):
@@ -11,6 +12,7 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length = 500, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
+    timezone = TimeZoneField(default='UTC') 
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
