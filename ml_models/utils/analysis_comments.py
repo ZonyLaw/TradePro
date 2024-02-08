c71b884f54bf4a48c9a2257a9d6ccba256091021
yuyizhong <sunny_law@hotmail.com>
2024-02-08 23:44:06 +0000

    relocate the add ticker link

--- ml_models/utils/analysis_comments.py
+++ ml_models/utils/analysis_comments.py
@@ -75,21 +75,21 @@ def compare_version_results(model_dict1, model_dict2, model_dict3, prices_df, ar
                 
     elif current_trade1 == current_trade2:
         comment = (
-            f"Both 4hr models predict the same {current_trade2}{next_line}"
-            f"{market_comments}.{next_line}"
-        )
-        send_email = 1
-    
-    elif current_trade2 == current_trade3:
-        comment = (
-            f"Oscillation possible: 4hr model and 1hr model predict the same {current_trade2}.{next_line}"
+            f"Oscillation: Both 4hr models predict the same {current_trade2}{next_line}"
             f"{market_comments}.{next_line}"
         )
         send_email = 0
+    
+    elif current_trade2 == current_trade3:
+        comment = (
+            f"4hr model and 1hr model predict the same {current_trade2}.{next_line}"
+            f"{market_comments}.{next_line}"
+        )
+        send_email = 1
         
     elif current_trade1 == current_trade3:
         comment = (
-            f"Oscillation possible: 4hr LAGGED model and 1hr model predict the same {current_trade2}{next_line}"
+            f"Oscillation: 4hr LAGGED model and 1hr model predict the same {current_trade2}{next_line}"
             f"{market_comments}.{next_line}"
         )
         send_email = 0
