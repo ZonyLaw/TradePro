7f20d0f7e79af6e90645486143f305d65596b767
yuyizhong <sunny_law@hotmail.com>
2024-02-14 21:52:18 +0000

    add reset password process

--- tradepro/urls.py
+++ tradepro/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tickers/', include('tickers.urls')),
     path('prices/', include('prices.urls')),
     path('ml_models/', include('ml_models.urls')),
     path('', include('users.urls')),
+    
+
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -11,4 +12,13 @@ urlpatterns = [
     path('profile/<str:pk>', views.userProfile, name="user-profile"),
     path('edit-profile/', views.editProfile, name="edit-profile"),
     path('account/', views.userAccount, name="account"),
+    
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="users/reset_password.html"),
+         name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="users/reset_password_sent.html"),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="users/reset.html"),
+         name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="users/reset_password_complete.html"),
+         name="password_reset_complete"),
 ]
