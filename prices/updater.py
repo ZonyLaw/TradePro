442e62b47e3592e042944fbd0da2efb551a4490c
yuyizhong <sunny_law@hotmail.com>
2024-03-23 07:26:25 +0000

    add a log command for updater

--- prices/updater.py
+++ prices/updater.py
@@ -4,21 +4,25 @@ from django.conf import settings
 from prices.IG_session.run_IG import run_IG
 from tradepro.utils.email_alert import email_alert
 from ml_models.utils.predictive_analysis import run_model_predictions
-
-
 # https://stackoverflow.com/questions/69387768/running-apscheduler-cron-at-certain-interval-combining-minutes-and-seconds
 
+import logging
+import os
+
+current_directory = os.getcwd()
+logging.basicConfig(filename=f'{current_directory}/media/logfile.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def start():
+    logging.info("Starting scheduler")
     print("starting scheduler")
     ticker = "USDJPY"
-    print(type(ticker))
     scheduler = BackgroundScheduler()
    
     current_time = datetime.now()
     print("updater date and time", current_time)
     
-    
+     
+    logging.info("Updater date and time: %s", current_time)
     # Method 1: call the scheduler when app is loaded
     # scheduler.add_job(run_procedure_sequence, 'date', run_date=datetime.now() + timedelta(seconds=1))
 
@@ -38,9 +42,11 @@ def start():
     
     scheduler.start()
     print("ending scheduler")
+    logging.info("Ending scheduler")
 
 
 def run_procedure_sequence():
+    logging.info("Inside the procedure sequence")
     print("inside the procedure sequence!!!!")
     tickers = ["USDJPY"]
     for ticker in tickers:
